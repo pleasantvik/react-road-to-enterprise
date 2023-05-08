5e8544b25ef1413fe80c996259aca77d1ee71515
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-05-08 08:39:11 +0100

    project initialization and installation of dependencis

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      ...colors,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
